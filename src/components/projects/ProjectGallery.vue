c9c62f0580a64fd715b07f7e0df338b5617d9424
nazmeaiman97 <72779691+nazmeaiman97@users.noreply.github.com>
2022-07-11 15:59:31 +0800

    Update ProjectGallery.vue

--- src/components/projects/ProjectGallery.vue
+++ src/components/projects/ProjectGallery.vue
@@ -1,21 +1,27 @@
 <script>
 export default {
-	props: ['projectImages'],
+  props: ["projectImages"],
 };
 </script>
 
 <template>
-	<div class="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12">
-		<div
-			class="mb-10 sm:mb-0"
-			v-for="projectImage in projectImages"
-			:key="projectImage.id"
-		>
-			<img
-				:src="projectImage.img"
-				class="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
-				alt="{{ projectImage.title }}"
-			/>
-		</div>
-	</div>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12 place-items-center"
+    style="text-align: -webkit-center !important"
+  >
+    <div
+      class="mb-10 sm:mb-0"
+      style="text-align: -webkit-center"
+      v-for="projectImage in projectImages"
+      :key="projectImage.id"
+    >
+      <div class="center" style="text-align: -webkit-center">
+        <img
+          :src="projectImage.img"
+          class="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
+          alt="{{ projectImage.title }}"
+        />
+      </div>
+    </div>
+  </div>
 </template>
